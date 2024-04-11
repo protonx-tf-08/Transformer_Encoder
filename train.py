8b45b6b6aea8d33e8d6ff2855e60e885def745b9
Dungtd <dungtrandinh513@gmail.com>
2024-04-11 23:55:02 +0700

    File img

--- train.py
+++ train.py
@@ -13,15 +13,13 @@ if __name__ == "__main__":
     # FIXME
     # Arguments users used when running command lines
 
-    parser.add_argument("--data_dir", default='imdb.npz', type =str)
-    parser.add_argument("--output_dir",default="output",type =str)
-    parser.add_argument("--batch-size", default=32, type=int)
+    parser.add_argument("--batch_size", default=32, type=int)
     parser.add_argument("--epochs", default=10, type=int)
     parser.add_argument("--d_model", default=128, type=int)
     parser.add_argument("--max_length", default=200,type=int)
-    parser.add_argument("--input_vocab_size", default=10000, type=int)
+    parser.add_argument("--vocab_size", default=10000, type=int)
     parser.add_argument("--dff", default=512, type=int)
-    parser.add_argument("--num_heads", default=4, type=int)
+    parser.add_argument("--num_heads", default=2, type=int)
     parser.add_argument("--num_encoder_layers",default = 2, type=int)
     parser.add_argument("--learning_rate",default=0.001,type=float)
     parser.add_argument("--dropout-rate", default=0.1, type = float)
@@ -37,11 +35,11 @@ if __name__ == "__main__":
     # FIXME
     # Project Description
 
-    print('---------------------Welcome to ${ProtonX Transformer Encoder}-------------------')
-    print('Github: ${Dungfx15018}')
-    print('Email: ${duntrandinh513@gmail.com}')
+    print('---------------------Welcome to ProtonX Transformer Encoder-------------------')
+    print('Github: Dungfx15018')
+    print('Email: dungtrandinh513@gmail.com')
     print('---------------------------------------------------------------------')
-    print('Training ${name} model with hyper-params:') # FIXME
+    print('Training Transformer Classifier model with hyper-params:')
     print('===========================')
 
     for arg, value in vars(args).items():
@@ -49,7 +47,7 @@ if __name__ == "__main__":
 
 
     # Process data
-    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words= args.input_vocab_size)
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=args.vocab_size)
     x_train = pad_sequences(x_train, maxlen = args.max_length)
     x_test = pad_sequences(x_test, maxlen= args.max_length)
 
@@ -58,7 +56,7 @@ if __name__ == "__main__":
         num_encoder_layers = num_encoder_layers,
         d_model = d_model,
         num_heads = args.num_heads,
-        input_vocab_size = args.input_vocab_size,
+        input_vocab_size = args.vocab_size,
         maximum_position_encoding = args.max_length,
         dff = args.dff,
         rate = args.dropout_rate
@@ -67,7 +65,7 @@ if __name__ == "__main__":
     model.compile(loss = tf.keras.losses.BinaryCrossentropy(), optimizer= tf.keras.optimizers.Adam(), metrics = ['BinaryAccuracy'] )
 
     # Train the model
-    model.fit(x_train, y_train, epochs = args.epochs, batch_size = args.batch_size, validation_data = (x_test, y_test))
+    model.fit(x_train, y_train, batch_size=args.batch_size, epochs=args.epochs, validation_data=(x_test,y_test))
     # Evaluate the model
     test_loss,test_acc = model.evaluate(x_test, y_test)
 
